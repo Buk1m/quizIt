8df6b4c284623b58cc32d2101d05f80705e87a61
Bartosz Kudra <bkudra@hotmail.com>
2018-12-15 20:08:46 +0100

    resolved conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { GenerateQuizComponent } from './generate-quiz/generate-quiz.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +10,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { LoginComponent } from './login/login.component';
-import { GenerateQuizComponent } from './generate-quiz/generate-quiz.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +19,6 @@ import { GenerateQuizComponent } from './generate-quiz/generate-quiz.component';
     SidebarComponent,
     TopbarComponent,
     LoginComponent,
-    GenerateQuizComponent
   ],
   imports: [
     BrowserModule,
