06221e3bf0734e90513947c99ea4a14b33af4a5c
Kacper Hałuszczak <anakinud@o2.pl>
2018-12-16 07:22:51 +0100

    Merge branch 'master' of https://github.com/Buk1m/quizIt
    # Conflicts:
    #	src/app/app-routing.module.ts
    #	src/app/app.module.ts

--- src/app/quiz-questions/question-answers/question-answers.component.ts
+++ src/app/quiz-questions/question-answers/question-answers.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Question} from '../../model/question';
 
 @Component({
@@ -6,24 +6,33 @@ import {Question} from '../../model/question';
   templateUrl: './question-answers.component.html',
   styleUrls: ['./question-answers.component.css']
 })
-export class QuestionAnswersComponent {
+export class QuestionAnswersComponent implements OnInit {
 
   @Input()
-  question: Question = {
-    content: "ELOELO",
-    urlImage: "",
-    questionType: "Test",
-    answers: [
-      {content: "Test1", isCorrect: false},
-      {content: "Test2", isCorrect: false},
-      {content: "Test3", isCorrect: true},
-    ]
-  };
+  question: Question;
 
   @Output()
-  selectedAnswers: number[] = [];
+  correctness: EventEmitter<number> = new EventEmitter<number>();
 
-  moveOn() {
+  checkedCorrect: number;
 
+  correctAmount: number;
+
+  ngOnInit() {
+    let result = 0;
+    for (let answer of this.question.answers) {
+      if (answer.isCorrect === true) {
+        result++;
+      }
+    }
+    this.correctAmount = result;
+  }
+  checkAnswer($event: any) {
+    if ($event.selected === 0) {
+      this.checkedCorrect--;
+    } else if ($event.selected === 1) {
+      this.checkedCorrect++;
+    }
+    this.correctness.emit(this.checkedCorrect / this.correctAmount);
   }
 }
--- src/app/quiz-questions/quiz-questions.component.ts
+++ src/app/quiz-questions/quiz-questions.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {Quiz} from '../model/quiz';
 import {AuthenticationService} from '../authentication.service';
 import {QuizService} from '../quiz.service';
@@ -13,11 +13,31 @@ export class QuizQuestionsComponent implements OnInit {
   @Input()
   quiz: Quiz;
 
+  @Output() score: EventEmitter<number> = new EventEmitter<number>();
+
+  time = 0;
+  interval;
+
   constructor(private quizService: QuizService,
               private authService: AuthenticationService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.startTimer();
   }
 
+  startTimer() {
+    this.interval = setInterval(() => {
+      this.time++;
+    }, 1000);
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+
+  countScore($event: number) {
+    this.score.emit($event / this.time);
+  }
 }
